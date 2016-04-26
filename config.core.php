441eb6417fc81a482d21c04cced58da3d82b0d5a
Dennis Grundelius <dgrundelius@gmail.com>
2016-04-27 00:08:23 +0200

    Added files via upload

---
+++ config.core.php
@@ -0,0 +1,35 @@
+<?php
+
+// Paths
+define('DENGRU_CORE_PATH', dirname(__FILE__) . '/core');
+define('DENGRU_DASH_PATH', dirname(__FILE__) . '/dashboard');
+//define('DENGRU_MANAGER_URL', '/dashboard');
+define('DENGRU_TEMPLATE_PATH', DENGRU_CORE_PATH . '/tpl');
+
+// Base constants
+define('DENGRU_SITE_PROTOCOL', 'http://');
+define('DENGRU_SITE_HOST', 'dengru');
+define('DENGRU_SITE_URL', DENGRU_SITE_PROTOCOL . DENGRU_SITE_HOST);
+
+// Dashboard constants
+define('DENGRU_DASH_PROTOCOL', 'http://');
+define('DENGRU_DASH_HOST', 'dashboard.dengru');
+define('DENGRU_DASH_URL', DENGRU_DASH_PROTOCOL . DENGRU_DASH_HOST);
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'degrun');
+define('DB_USER', 'degrun');
+define('DB_PASS', 'eKcGZr59zAa2BEWU');
+
+define('SQLITE_FILE', './test.sdb');
+
+// Debugging
+define('DEBUG', true);
+
+// Session constants
+define('_SESSION_SALT', $_SERVER['HTTP_HOST']);
+define('_SESSION_NAME', preg_replace('#[^a-z0-9]#i', '', $_SERVER['HTTP_HOST']));
+define('_SESSION_DIR', str_replace('.', '_', $_SERVER['HTTP_HOST']) . '_sessiondata');
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+@include(dirname(__FILE__) . '/config.core.php');
+
+@include_once(DENGRU_CORE_PATH . '/dengru.class.php');
+if (!@include_once(DENGRU_CORE_PATH . '/dengru.class.php')) {
+	header('HTTP/1.1 503 Service Unavailable');
+	echo "<html><title>Error 503: Site temporarily unavailable</title><body><h1>Error 503</h1><p>Site temporarily unavailable</p></body></html>";
+	exit();
+} 
+
+$dengru = new Dengru();
+
+$dengru->initialize();// Initialize DengruCMS
+?>
